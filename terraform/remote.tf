db942bc22022c4b9570ec96e112d7b03ce23f0b4
Vivian <okeyogechivivian@gmail.com>
2025-01-15 19:29:23 +0100

    testing

--- terraform/remote.tf
+++ terraform/remote.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ogechi-test-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-east-1" # Replace with your bucket's region
-    encrypt        = true
+    bucket  = "ogechi-test-bucket"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
